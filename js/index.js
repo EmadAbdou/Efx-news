73c0fb79223483679429ed985aeb2f09a9efdd52
Emad Abdou <emad_abdou7@yahoo.com>
2018-10-31 15:10:50 +0200

    Complete Responsiveness
    - Make some edits on responsiveness

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
+// Making Side Menu Button Appear Or Disappear in Index.html Page
 $(document).ready(function() {
   $(".side-menu-button").on("click", function() {
     $(".side-menu").toggleClass("show-menu");
   });
+});
+
+// Making All Collapses Close When One Open
+var $myGroup = $("#table-data-group");
+$myGroup.on("show.bs.collapse", ".collapse", function() {
+  $myGroup.find(".collapse.show").collapse("hide");
 });
